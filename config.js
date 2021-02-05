b3d1c7882bc3ddb4adcc1e8a3b0fad3909f25eac
Mikebezeau <mikebezeau@gmail.com>
2021-02-05 15:53:49 -0500

    include config file

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_DB: 'mongodb+srv://classsed:rDqtDVq9SKTXyVb@clusterblackbird.1mntz.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'something secret, encode token'
+}
